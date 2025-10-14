ef980d4f8ef134181011bc0a52fa2ce7acdf77a0
Kenai <151666143+Kenainn@users.noreply.github.com>
2025-10-13 19:24:17 -0600

    resuelto el 2

--- 06 Algoritmia/js/algoritmia.js
+++ 06 Algoritmia/js/algoritmia.js
@@ -10,6 +10,38 @@ function problema1(){
     document.getElementById("p1-output").textContent = palabras.join(" ");
 }
 
+//Problema 2
+function problema2(){
+    var p2_x1 = document.querySelector("#p2_x1").value;
+    var p2_x2 = document.querySelector("#p2_x2").value;
+    var p2_x3 = document.querySelector("#p2_x3").value;
+    var p2_x4 = document.querySelector("#p2_x4").value;
+    var p2_x5 = document.querySelector("#p2_x5").value;
+
+    var p2_y1 = document.querySelector("#p2_y1").value;
+    var p2_y2 = document.querySelector("#p2_y2").value;
+    var p2_y3 = document.querySelector("#p2_y3").value;
+    var p2_y4 = document.querySelector("#p2_y4").value;
+    var p2_y5 = document.querySelector("#p2_y5").value;
+
+    //creamos los vectores
+    var v1 = [p2_x1,p2_x2,p2_x3,p2_x4,p2_x5]
+    var v2 = [p2_y1,p2_y2,p2_y3,p2_y4,p2_y5]
+
+    v1 = v1.sort(function(a,b){return b-a})
+    v2 = v2.sort(function(a,b){return b-a})
+
+    v2 = v2.reverse();
+
+    var p2_producto = 0;
+    for (var i=0; i<v1.length; i++){
+        p2_producto += v1[i] * v2[i]
+        
+    }
+
+    document.querySelector("#p2_output").innerHTML = "El producto escalar minimo es: " + p2_producto;
+}
+
 // Problema 3
 function problema3(){
     let input = document.getElementById("p3-input").value.trim();
